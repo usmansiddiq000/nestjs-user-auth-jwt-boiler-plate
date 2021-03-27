67a38da7e6df122f4ab711493d43d5fe85a583f8
usman siddiq <usmansiddiq000@gmail.com>
2021-03-27 23:12:52 +0500

    auth-module-added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { AppService } from './app.service';
 
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot('mongodb://localhost/nest'),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
